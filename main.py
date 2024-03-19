168f64eff22d700046c598354a2ab93720f57552
Zach Hom <123410472+zxhom@users.noreply.github.com>
2024-03-19 23:36:44 +0000

    final1

--- main.py
+++ main.py
@@ -1,21 +1,49 @@
 import random, time
 import tabulate
 
+#random.seed(1)
+# for repeatability
+
+
 def ssort(L):
     ### selection sort
     if (len(L) == 1):
-        return(L)
+        return (L)
     else:
         m = L.index(min(L))
-        print('selecting minimum %s' % L[m])       
+        print('selecting minimum %s' % L[m])
         L[0], L[m] = L[m], L[0]
         print('recursively sorting L=%s\n' % L[1:])
-        return [L[0]] + selection_sort(L[1:])
-        
+        return [L[0]] + ssort(L[1:])
+    #return [L[0]] + selection_sort(L[1:])
+
+
 def qsort(a, pivot_fn):
-    ## TO DO
-    pass
-    
+    ### Fixed Pivot Implementation
+    #p = a[0]
+
+    ### Random Pivot Implementation
+    #p = random.choice(a)
+
+    # Base case 0 or 1??
+    if len(a) <= 1:
+        return a
+    else:
+        p = pivot_fn(a)  # integer element
+        l = list(filter(lambda x: x < p, a))  # unsorted left list
+        m = list(filter(lambda x: x == p, a))  # pivot list
+        r = list(filter(lambda x: x > p, a))  # unsorted right list
+        return qsort(l, pivot_fn) + m + qsort(r, pivot_fn)
+
+
+def first_pivot(a):  # added
+    return a[0]
+
+
+def random_pivot(a):  # added
+    return random.choice(a)
+
+
 def time_search(sort_fn, mylist):
     """
     Return the number of milliseconds to run this
@@ -39,7 +67,9 @@ def time_search(sort_fn, mylist):
     return (time.time() - start) * 1000
     ###
 
-def compare_sort(sizes=[100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 100000]):
+
+def compare_sort(sizes=[50, 100, 150, 200, 250, 300, 350, 400, 450, 500]):
+    #sizes=[100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 100000]
     """
     Compare the running time of different sorting algorithms.
 
@@ -50,32 +80,50 @@ def compare_sort(sizes=[100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 10
       for each method to run on each value of n
     """
     ### TODO - sorting algorithms for comparison
-    qsort_fixed_pivot = # 
-    qsort_random_pivot = #
-    tim_sort = #
+
+    qsort_fixed_pivot = lambda a: qsort(a, first_pivot)  # changed
+    qsort_random_pivot = lambda a: qsort(a, random_pivot)  # changed
+    tim_sort = lambda a: sorted(a)  # compare timsort last
+    selection_sort = lambda a: ssort(a)
+
     result = []
     for size in sizes:
         # create list in ascending order
         mylist = list(range(size))
+        #random.shuffle(mylist)
+        # need to shuffle before sorting to eliminate recursion error; compiler issue
         # shuffles list if needed
         #random.shuffle(mylist)
         result.append([
             len(mylist),
             time_search(qsort_fixed_pivot, mylist),
             time_search(qsort_random_pivot, mylist),
+            # time_search(selection_sort, mylist),
+            time_search(tim_sort, mylist)
         ])
     return result
     ###
 
+
 def print_results(results):
     """ change as needed for comparisons """
-    print(tabulate.tabulate(results,
-                            headers=['n', 'qsort-fixed-pivot', 'qsort-random-pivot'],
-                            floatfmt=".3f",
-                            tablefmt="github"))
+    print(
+        tabulate.tabulate(results,
+                          headers=[
+                              'n', 'qsort-fixed-pivot', 'qsort-random-pivot',
+                              'tim-sort'
+                          ],
+                          floatfmt=".3f",
+                          tablefmt="github"))
+
 
 def test_print():
     print_results(compare_sort())
 
+
 random.seed()
 test_print()
+
+#
+#
+# print(qsort(testlist, random_pivot))
--- recitation-05.md
+++ recitation-05.md
@@ -44,12 +44,52 @@ different list sizes. The `print_results` function in `main.py` gives
 a table of results, but feel free to use code from Lab 1 to plot
 the results as well. 
 
+*Dr. Ding told us to ignore ssort. 
+
+Quicksort and Timsort both have complexity of O(nlogn). Their performance  aligns with the asymptotic bounds, running faster than the worst case for these lists given randomly sorted lists. 
+
+For random permutations of specified sizes, random and fixed pivots ran with similar time complexities of O(nlogn) but Timsort was the fastest although Timsort also has a complexity of O(nlogn). 
+
+For sorted permutations of specified sizes, random pivot ran significantly faster than fixed pivot with Timsort still being the fastest. Qsort with a random pivot had approximately the same runtime for both permutation types while Qsort with a fixed pivot ran significantly slower for sorted permutations of specified sizes. Timsort was the only algorithm that ran noticeably faster for sorted permutations of specified sizes than for random permutations of specified sizes. 
+
+Random pivot generally sorts faster than fixed pivot as we expected. Random permutations of specified list sizes yielded slower performance from both pivot types of quicksort.
+
+Random Permutations of Specified Sizes
+|      n |   qsort-fixed-pivot |   qsort-random-pivot |   tim-sort |
+|--------|---------------------|----------------------|------------|
+|    100 |               7.603 |                0.854 |      0.016 |
+|    200 |               1.345 |                1.404 |      0.231 |
+|    500 |               5.310 |                2.895 |      0.060 |
+|   1000 |              10.964 |                8.960 |      0.133 |
+|   2000 |              13.533 |              285.002 |      0.364 |
+|   5000 |             348.991 |              495.324 |      1.144 |
+|  10000 |             551.490 |             1114.636 |      2.203 |
+|  20000 |            1991.493 |             1855.082 |     18.207 |
+|  50000 |            5850.204 |             6300.409 |    257.235 |
+| 100000 |           13720.633 |            13749.907 |    239.236 |
+
+Sorted Permutations of Specified Sizes
+|   n |   qsort-fixed-pivot |   qsort-random-pivot |   tim-sort |
+|-----|---------------------|----------------------|------------|
+|  50 |               2.633 |                0.409 |      0.005 |
+| 100 |               3.539 |                0.871 |      0.005 |
+| 150 |               7.235 |               47.125 |      0.007 |
+| 200 |               7.561 |                0.844 |      0.007 |
+| 250 |              10.695 |                1.253 |      0.007 |
+| 300 |              16.266 |                2.975 |      0.013 |
+| 350 |              76.971 |                2.276 |      0.017 |
+| 400 |              84.152 |                1.928 |      0.009 |
+| 450 |              39.623 |                2.038 |      0.020 |
+| 500 |             160.105 |                2.680 |      0.011 |
 
 
 **1c)**
 
 Python uses a sorting algorithm called [*Timsort*](https://en.wikipedia.org/wiki/Timsort), designed by Tim Peters. Compare the fastest of your sorting implementations to the Python
 sorting function `sorted`, conducting the tests in 1b above. 
+
+Timsort and Qsort with a random pivot were the top 2 fastest algorithms; however, timsort consistently ran faster than Qsort by a factor of ~100. 
+
 
 
 
